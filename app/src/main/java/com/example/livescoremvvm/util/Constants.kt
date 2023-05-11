ba011a2505331a0dd381fd318041d0a75f80482a
SyntaxLexx <spencermainaa@gmail.com>
2023-05-11 19:04:11 +0300

    feat: updated baseUrl to use online Json-Server

--- app/src/main/java/com/example/livescoremvvm/util/Constants.kt
+++ app/src/main/java/com/example/livescoremvvm/util/Constants.kt
@@ -1,9 +1,10 @@
 package com.example.livescoremvvm.util
 
-const val BASE_URL = "http://10.0.2.2:3000"
+//const val BASE_URL = "http://10.0.2.2:3000"
+const val BASE_URL = "https://my-json-server.typicode.com/syntaxlexx/jetpack-compose-mvvm-livescore-app/"
 
-const val GET_INPLAY_FIXTURES = "/inplay"
+const val GET_INPLAY_FIXTURES = "inplay"
 
-const val GET_UPCOMING_MATCHES = "/upcoming"
+const val GET_UPCOMING_MATCHES = "upcoming"
 
 const val ACCESS_TOKEN = "Bearer "

5ceace77f5645697141e777de1bb22f436928980
SyntaxLexx <spencermainaa@gmail.com>
2023-05-11 15:54:12 +0300

    feat: bypass network security for 10.0.2.2 for android emulator

--- app/src/main/java/com/example/livescoremvvm/util/Constants.kt
+++ app/src/main/java/com/example/livescoremvvm/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.livescoremvvm.util
 
-const val BASE_URL = "http://localhost:3000"
+const val BASE_URL = "http://10.0.2.2:3000"
 
-const val GET_INPLAY_FIXTURES = "/v2/inplay"
+const val GET_INPLAY_FIXTURES = "/inplay"
 
 const val ACCESS_TOKEN = "Bearer "
